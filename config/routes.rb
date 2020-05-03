e4ea6b94d772fe257eb0579b4bab57f5706640f2
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-05-03 20:02:59 +0900

    install-gemrefile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #ログイン後、一覧画面(index)に移動
+  #deviseではログイン認証後、成功すれば、rootパスにリダイレクトされる
+  root 'post_images#index'
 end
