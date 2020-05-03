5373be18a1aeeb0b744c2ebe3532ffa9936b0960
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-05-03 14:50:22 +0900

    add_sing-up

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+  validates :name, presence: true, length:{in: 2..20}
+  # def email_required?
+  #   false
+  # end
+
+
+  # def email_changed?
+  #   false
+  # end
+
+end
