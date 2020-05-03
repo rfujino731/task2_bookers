2e21b9d36a85241d69fdb3cffc255283cfcb4a0e
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-05-03 21:27:22 +0900

    add_model_prfileimage add_home-index

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #ログイン後、一覧画面(index)に移動
   #deviseではログイン認証後、成功すれば、rootパスにリダイレクトされる
-  root 'post_images#index'
+  root 'homes#index'
+
+  resources :homes, only: [:new, :create, :index, :show]
 end
