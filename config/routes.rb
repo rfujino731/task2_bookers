e3702bbdaf8c7ffea7bc810883efb19c3267cc49
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-05-10 12:34:56 +0900

    solution-error

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'homes#show' 
+  root to: 'homes#show'
 
   # ,as: 'show_home'
   devise_for :users
 
-  get "about" => "homes#about" , as: 'about_home'
+  get "/home/about" => "homes#about" , as: 'about_home'
 
-  resources :homes, only: [:show, :edit, :update, :index, :create]
+  # resources :homes, only: [:edit, :update, :index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'homes' => 'homes#show'
 
